89857c36633f6b2981c056be6bedbbc5353401d1
Chris Kim <kim.junehyung@gmail.com>
2013-08-27 23:16:42 -0400

    Ch6

---
+++ CH6.rb
@@ -0,0 +1,10 @@
+# Angry boss - "What do you want?" You answer. Boss yeslls and fire you.
+puts "What do you want?"
+your_want = gets.chomp
+puts "WHADDAYA MEAN \"I WANT A " + your_want.upcase + "?!?\" YOU'RE FIRED!!"
+# Table of Contents - ljust & rjust
+puts "Table of Contents".rjust(40)
+puts
+puts "Chapter 1: Getting Started".ljust(50) + "page 1".ljust(7)
+puts "Chapter 2: Numbers".ljust(50) + "page 9".ljust(7)
+puts "Chapter 3: Letters".ljust(50) + "page 13".ljust(7)
