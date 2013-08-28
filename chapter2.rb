a47cadaacbae6d9fe81430af2a33f73c313e1b40
Chris Kim <kim.junehyung@gmail.com>
2013-08-27 22:14:42 -0400

    chapter 2 answers

---
+++ chapter2.rb
@@ -0,0 +1,9 @@
+# Chapter 2. 
+# Hours in a year.
+puts 24*365
+# Minutes in a decade.
+puts 60*24*365*10
+# My age in seconds
+puts 27*365*24*3600
+# 1160 million seconds in years
+puts 1160000000/(365*24*3600)
